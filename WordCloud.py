c4f4dc9a9b006b556be0c869895dc97949fec756
Ritvik19 <rastogiritvik99@gmail.com>
2018-07-22 09:31:59 +0530

    New Scripts Added

--- WordCloud.py
+++ WordCloud.py
@@ -8,11 +8,16 @@ text = pyperclip.paste()
 w = int(sys.argv[1])
 h = int(sys.argv[2])
 bgc = sys.argv[3]
+if len(sys.argv) == 5:
+    mw = int(sys.argv[4])
+else:
+    mw = 200
 stopwords = set(STOPWORDS)
 wordcloud = WordCloud(width = w, height = h,
                 background_color =bgc,
                 stopwords = stopwords,
-                min_font_size = 10).generate(text)
+                min_font_size = 10,
+                max_words = mw).generate(text)
 
 # plot the WordCloud image
 plt.figure(figsize = (w//100, h//100), facecolor = None)
